fb02203dfad9bdff3a827bd67f57f874f4227102
JustAnatolyDev <119749324+Sskame1@users.noreply.github.com>
2023-10-16 08:46:20 +0900

    upd

--- back/main.py
+++ back/main.py
@@ -1,10 +1,10 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import Session
 from pydantic import BaseModel
-
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm, HTTPBasicCredentials
 app = FastAPI(title="Backend Anifriend")
 
 # Создание подключения к базе данных
@@ -34,6 +34,17 @@ Base.metadata.create_all(bind=engine)
 
 from sqlalchemy.orm import session
 
+@app.post("/login")
+def login(credentials: HTTPBasicCredentials):
+    name = credentials.username
+    password = credentials.password
+
+    db = SessionLocal()
+    user = db.query(User).filter(User.name == name).first()
+
+    if not user or not password == user.password:
+        raise HTTPException(status_code=401, detail="Неверные учетные данные")
+    return {"message": "Успешная аутентификация"}
 
 # Функция добавления пользователя в базу данных
 @app.post('/users')
